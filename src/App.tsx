3a8c3e6c26fcd2a87c9c966d853e16203c98ab0a
jim9112 <jim9112@gmail.com>
2021-03-08 16:43:31 -0500

    add json file and search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Clinician Administration Page</h1>
+      <h1 className="text-center">Clinician Administration Page</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" placeholder="search" />
+    </div>
+  );
+};
+
+export default SearchBar;
